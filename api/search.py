da2293965f3da3e78dd9f5517aa1ac2b617c72a9
Davyd Viniarskyi <davydviniarskyi.dev@gmail.com>
2023-10-11 16:21:10 +0300

    update gpt model

--- api/search.py
+++ api/search.py
@@ -74,7 +74,7 @@ def get_answer(query: str, paragraphs: list):
     """
 
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-4",
         messages=[
             {
                 "role": "user",
